0a9b4196ca044280547ff3658cda8f9a3f4716ab
jackenelson <114086517+jackenelson@users.noreply.github.com>
2023-09-12 15:22:36 -0600

    jack

--- Wordle.py
+++ Wordle.py
@@ -9,6 +9,7 @@ import random
 
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR
 
 def wordle():
 
@@ -17,7 +18,13 @@ def wordle():
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
-
+    
+    newWord = random.choice(FIVE_LETTER_WORDS)
+    print(newWord)
+    rows = N_ROWS
+    cols = N_COLS
+    for n in (range(0, len(newWord))):
+        gw.set_square_letter(0, n, newWord[n])
 # Startup code
 
 if __name__ == "__main__":
