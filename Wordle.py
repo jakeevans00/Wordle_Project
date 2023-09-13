3724cdbb408f050c2cdb72e7d5b9e8154584e88b
jakeevans <ejake370@gmail.com>
2023-09-12 21:23:47 -0600

    Updates row if guess is real word

--- Wordle.py
+++ Wordle.py
@@ -12,23 +12,34 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 from WordleGraphics import CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR
 
 def wordle():
+    rows = N_ROWS
+    cols = N_COLS
+    guess = 0
+    win = False
+    wordToGuess = random.choice(FIVE_LETTER_WORDS)
+    print(wordToGuess)
 
     def enter_action(s):
         print(s)
         if s.lower() in FIVE_LETTER_WORDS:
             gw.show_message("Real word")
+            gw.set_current_row(gw.get_current_row() + 1)
         else:
             gw.show_message("Not a real word")
+        
+        print(gw.get_current_row())
+        return
 
     gw = WordleGWindow()
+    print(gw.get_current_row())
+
+
     gw.add_enter_listener(enter_action)
+
     
-    newWord = random.choice(FIVE_LETTER_WORDS)
-    print(newWord)
-    rows = N_ROWS
-    cols = N_COLS
-    for n in (range(0, len(newWord))):
-        gw.set_square_letter(0, n, newWord[n])
+    
+    for n in (range(0, len(wordToGuess))):
+        gw.set_square_letter(gw.get_current_row(), n, wordToGuess[n])
 # Startup code
 
 if __name__ == "__main__":
