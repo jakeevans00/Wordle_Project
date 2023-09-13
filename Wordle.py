afec35567fded72368186532329052dc3c2939d5
jakeevans <ejake370@gmail.com>
2023-09-12 21:53:10 -0600

    Completed part of milestone 3. Colors in correct and present letters. Doesn't handle all edge cases

--- Wordle.py
+++ Wordle.py
@@ -16,14 +16,29 @@ def wordle():
     cols = N_COLS
     guess = 0
     win = False
-    wordToGuess = random.choice(FIVE_LETTER_WORDS)
+    # wordToGuess = random.choice(FIVE_LETTER_WORDS)
+    wordToGuess = 'glass'
+    tempList = wordToGuess
     print(wordToGuess)
 
     def enter_action(s):
         print(s)
         if s.lower() in FIVE_LETTER_WORDS:
             gw.show_message("Real word")
-            gw.set_current_row(gw.get_current_row() + 1)
+            
+            for n in range(0,len(wordToGuess)):
+
+                if gw.get_square_letter(gw.get_current_row(),n).lower() == wordToGuess[n]:
+                    gw.set_square_color(gw.get_current_row(),n,CORRECT_COLOR)
+                elif gw.get_square_letter(gw.get_current_row(), n).lower() in tempList:
+                    gw.set_square_color(gw.get_current_row(), n, PRESENT_COLOR)
+                else:
+                    gw.set_square_color(gw.get_current_row(), n, MISSING_COLOR)
+            
+            if s.lower() == wordToGuess:
+                gw.show_message("Congrats! Winner in " + str(gw.get_current_row()+1) + "!!!")
+            else:
+                gw.set_current_row(gw.get_current_row() + 1)
         else:
             gw.show_message("Not a real word")
         
@@ -31,15 +46,11 @@ def wordle():
         return
 
     gw = WordleGWindow()
-    print(gw.get_current_row())
-
 
     gw.add_enter_listener(enter_action)
 
-    
-    
-    for n in (range(0, len(wordToGuess))):
-        gw.set_square_letter(gw.get_current_row(), n, wordToGuess[n])
+    # for n in (range(0, len(wordToGuess))):
+    #     gw.set_square_letter(gw.get_current_row(), n, wordToGuess[n])
 # Startup code
 
 if __name__ == "__main__":
