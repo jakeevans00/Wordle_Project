81c83a65cbf5fe0174c0a52f3d964536eb169b89
jackenelson <114086517+jackenelson@users.noreply.github.com>
2023-09-13 11:04:20 -0600

    2nd

--- Wordle.py
+++ Wordle.py
@@ -12,19 +12,51 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 from WordleGraphics import CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR
 
 def wordle():
-
-    def enter_action(s):
-        gw.show_message("You have to implement this method.")
-
-    gw = WordleGWindow()
-    gw.add_enter_listener(enter_action)
-    
-    newWord = random.choice(FIVE_LETTER_WORDS)
-    print(newWord)
     rows = N_ROWS
     cols = N_COLS
-    for n in (range(0, len(newWord))):
-        gw.set_square_letter(0, n, newWord[n])
+    guess = 0
+    win = False
+    # wordToGuess = random.choice(FIVE_LETTER_WORDS)
+    wordToGuess = 'glass'
+    tempList = wordToGuess
+    lettersUsed = []
+    print(wordToGuess)
+
+    def enter_action(s):
+        print(s)
+        if s.lower() in FIVE_LETTER_WORDS:
+            gw.show_message("Real word")
+            
+            for n in range(0,len(wordToGuess)):
+                letterGuess = gw.get_square_letter(gw.get_current_row(),n).lower()
+                
+                if letterGuess == wordToGuess[n]:
+                    gw.set_square_color(gw.get_current_row(),n,CORRECT_COLOR)
+                    lettersUsed.append(letterGuess)
+                elif letterGuess in tempList:
+                    if letterGuess not in lettersUsed :
+                        gw.set_square_color(gw.get_current_row(), n, PRESENT_COLOR)
+                    lettersUsed.append(letterGuess)
+                else:
+                    gw.set_square_color(gw.get_current_row(), n, MISSING_COLOR)
+                    lettersUsed.append(letterGuess)
+            
+            if s.lower() == wordToGuess:
+                gw.show_message("Congrats! Winner in " + str(gw.get_current_row()+1) + "!!!")
+            else:
+                gw.set_current_row(gw.get_current_row() + 1)
+        else:
+            gw.show_message("Not a real word")
+        
+        print(gw.get_current_row())
+        return
+
+    gw = WordleGWindow()
+
+    gw.add_enter_listener(enter_action)
+
+    # for n in (range(0, len(wordToGuess))):
+    #     gw.set_square_letter(gw.get_current_row(), n, wordToGuess[n])
 # Startup code
 
 if __name__ == "__main__":
